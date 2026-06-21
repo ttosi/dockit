524208e25ba70728cf42a9303345efe7ee79813c
Tony Tosi <ttosi519@gmail.com>
2026-06-21 06:26:51 -0500

    Update create_password.js

--- api/create_password.js
+++ api/create_password.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt-nodejs";
 
 const salt = bcrypt.genSaltSync(10);
-const pass = bcrypt.hashSync("<PASSWRD>", salt);
+const pass = bcrypt.hashSync("<PASSWORD_TO_HASH>", salt);
 console.log(pass);
